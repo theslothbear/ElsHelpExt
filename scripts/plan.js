7433be9cfd80731cd903732db5130c4a147c8024
theslothbear <128232763+theslothbear@users.noreply.github.com>
2024-11-27 23:19:20 +0500

    Revert "Update FireFox to v.2.0"
    This reverts commit e7356dfb743fea784382bd8c373d26323fbf6579.

--- scripts/plan.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("addtask").addEventListener("click", addtask);
-function addtask(){
-	var n = document.querySelector('#number_tasks').value;
-	n-=-1; 
-	document.querySelector('#tasks').insertAdjacentHTML('beforeend', `<div><input type="checkbox" id="c${n}" class="c" /><div contenteditable="true" class="t" id="t${n}">Задача</div></div>`);
-	document.querySelector('#number_tasks').value = n;
-	document.querySelector(`#t${n}`).focus();
-}
